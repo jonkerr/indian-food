d6ff9cdff1969d811e8dadf9ca49cd972947c0c7
Jonathan Kerr <jon.kerr@gmail.com>
2024-10-15 17:27:36 -0700

    Moved some image processing code to a module to use for multiple datasets.

---
+++ image_prep.py
@@ -0,0 +1,46 @@
+import os
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+RANDOM_STATE = 42
+
+def get_img_df(path):
+    """
+    List the images under each folder and append the path onto each image name
+    """
+    # each folder is also the label name
+    labels = os.listdir(path)
+
+    # associate the image path with the label
+    image_dct = {}
+    for label in labels:
+        label_path = path + label
+        # Yes, the preferred method is os.path.join but it's ugly on Windows machines and 
+        # *NIX style paths seem to work just fine in python.
+        image_dct[label] = [label_path + '/' + img for img in os.listdir(label_path)]
+        
+    # create df and melt to have the column names (labels) in same row as image path
+    # apply a neat trick for dictionaries of unequal lengths
+    # https://stackoverflow.com/questions/19736080/creating-dataframe-from-a-dictionary-where-entries-have-different-lengths
+    return pd.DataFrame(dict([ (k,pd.Series(v)) for k,v in image_dct.items() ])).melt()\
+        .dropna().rename(columns={'variable':'label', 'value':'path'})
+
+
+def split(df, test_size):
+    # stratify on each label to ensure we don't introduce a class imbalance.
+    return train_test_split(df, test_size=test_size, random_state=RANDOM_STATE, stratify=df['label'])
+
+
+def train_val_test_split(df, test_size=0.2, val_size=0.2):    
+    # test validate size will be the combination of those two values
+    test_size_c = test_size+val_size    
+    # split
+    train, test_validate = split(df, test_size_c)
+    
+    # need to calcuate proportion of test_validate that is attributed to the test set
+    test_size_c = test_size / (test_size+val_size)
+    # split
+    test, validate = split(test_validate, test_size_c)
+    return train, test, validate
+
+
