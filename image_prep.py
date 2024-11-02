cfaec17f8963fee6370808a16a4016abf3b12925
Jonathan Kerr <jon.kerr@gmail.com>
2024-11-02 08:07:57 -0700

    Filter outliers

--- image_prep.py
+++ image_prep.py
@@ -2,18 +2,53 @@ import os
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
+
+
 RANDOM_STATE = 42
 
-def clean_df(df):
+def remove_outliers(indf):
     """
-    Insert cleaning code here
+    Borrowed some outlier detection from: https://www.kaggle.com/code/varsha300/transferlearning
     """
-    return df
+    import cv2
+    from scipy.stats import zscore
+    # Define a threshold for what we consider to be an outlier.
+    # We select 3 as that should capture 99.7% of the variance.
+    def z_filter(df, col, threshold=3):
+        return df[~((df[col] > threshold) | (df[col] < -threshold))]
+
+    df = indf.copy()
+    
+    # read images
+    df['image_gray'] = df.apply(lambda row: cv2.imread(row['path'], cv2.IMREAD_GRAYSCALE), axis=1)
+    # remove rows with any non-images
+    df = df.dropna(subset=['image_gray'])
+
+    # Filter image sizes based on zscore outliers
+    df['image_size'] = df.apply(lambda row: row['image_gray'].shape[0] * row['image_gray'].shape[1] , axis=1)
+    df['z_scores_size'] = zscore(df['image_size'])
+    df = z_filter(df, 'z_scores_size').copy()
+
+    # Calculate z-scores for the image quality using Laplacian variance
+    df['laplacian'] = df.apply(lambda row: cv2.Laplacian(row['image_gray'], cv2.CV_64F).var() , axis=1)
+    df['z_scores_laplacian'] = zscore(df['laplacian'])
+    df = z_filter(df, 'z_scores_laplacian')    
+    return df[['label','path']]
+
 
 def get_img_df(path):
     """
-    List the images under each folder and append the path onto each image name
+    List the images under each folder and append the path onto each image name.
+    Then remove outliers.
+    
+    Note: removing outliers is an expensive operation.  Cache results for a given path and use those if available.
     """
+    
+    # check for cache
+    cache_path = os.path.join(path, 'image_df.pkl.xz') 
+    if os.path.exists(cache_path):
+        return pd.read_pickle(cache_path) 
+    
     # each folder is also the label name
     labels = os.listdir(path)
 
@@ -30,8 +65,12 @@ def get_img_df(path):
     # https://stackoverflow.com/questions/19736080/creating-dataframe-from-a-dictionary-where-entries-have-different-lengths
     df = pd.DataFrame(dict([ (k,pd.Series(v)) for k,v in image_dct.items() ])).melt()\
         .dropna().rename(columns={'variable':'label', 'value':'path'})
-        
-    return clean_df(df)
+    df = remove_outliers(df)
+    
+    # cache results of outlier removal
+    df.to_pickle(cache_path)
+    
+    return df
 
 
 def split(df, test_size):
