4fc401d7e214889db931734c63dc760c17c500bd
sona-srri <144087645+sona-srri@users.noreply.github.com>
2024-12-04 16:01:44 -0500

    updated file to move more of the modular code from ui file to this one as functions.

--- ui_functions.py
+++ ui_functions.py
@@ -1,7 +1,13 @@
 from recommendation_models import filter_recipes
 import streamlit as st
 import numpy as np
+import pandas as pd
+from PIL import Image
+import tempfile
+import os
+from cv_predict import TastyFoodPredictor
 
+processed_df = pd.read_pickle("data/processed_recipes.pkl")
 
 # Function to reset session state
 def reset_session_state():
@@ -25,16 +31,133 @@ def reset_session_state():
     st.session_state['recommended_recipes'] = None
     st.session_state['user_inputs'] = None
     st.session_state['reset_trigger'] = True  # Set a trigger for resetting widgets
+    st.session_state['clear_image_triggered'] = True
 
-    print("Session state after reset:", st.session_state)  # Debugging step
+    # print("Session state after reset:", st.session_state)  # Debugging step
+
+def initialize_session_state():
+    if 'predicted_dish_name' not in st.session_state:
+        reset_session_state()
+    if 'uploaded_image' not in st.session_state:
+        st.session_state['uploaded_image'] = None
+        # reset_session_state()
+    if 'clear_image_triggered' not in st.session_state:
+        st.session_state['clear_image_triggered'] = True
+        # reset_session_state()
+    if 'prediction_done' not in st.session_state:
+        st.session_state['prediction_done'] = False
+        # reset_session_state()
+    if 'selected_recipe' not in st.session_state:
+        st.session_state['selected_recipe'] = "Select an option"
+        # reset_session_state()
 
 # Function to format dish names
 def format_dish_name(dish_name):
-    print('test1')
+    # print('test1')
     # Remove special characters (replace underscores with spaces)
     predicted_dish_name_for_filter = dish_name.replace("_", " ")
     formatted_name = dish_name.replace("_", " ").title()
     return formatted_name, predicted_dish_name_for_filter
+
+# function to display upload image option, clear image and/or predict dish name
+# this saves predicted dish name
+# perdicted dish name is formated in sentence case for the display, however it is saved in all lower case for filtering matching recipes
+def image_upload_and_prediction():
+
+    # Load the pre-trained image-based model
+    tasty_model = TastyFoodPredictor()
+
+    if st.session_state.get('uploaded_image') is None:
+        # st.session_state['clear_image_triggered'] = False
+        uploaded_image = st.file_uploader("Upload an Image (optional)", type=["jpg", "jpeg", "png"], key="image_uploader")
+        if uploaded_image is not None:
+            st.session_state['uploaded_image'] = uploaded_image
+            # st.session_state['clear_image_triggered'] = False  # Reset clear trigger
+
+    # Display uploaded image if present in session state
+    if st.session_state.get('uploaded_image') is not None:
+        st.markdown("### Uploaded Image:")
+        image = Image.open(st.session_state['uploaded_image'])
+        st.image(image, caption="Uploaded Image", use_column_width=True)
+
+        # Clear Image Button
+        if st.button("Clear Image"):
+            reset_session_state()
+
+        # Instructions for the button (to manage the bug)
+        st.markdown(
+            "<p style='font-size: small; color: gray;'><em>*Please click this button twice with a 2-second interval to clear the image.*</em></p>",
+            unsafe_allow_html=True
+        )
+
+        # preidict dish name
+        if st.button("Identify Dish Name"):
+        # Save the uploaded image as a temporary file
+            with tempfile.NamedTemporaryFile(suffix=".jpg", delete=False) as temp_image_file:
+                temp_image_path = temp_image_file.name
+                image.save(temp_image_path)
+
+            try:
+                # Predict the dish name using the path to the temporary image
+                predicted_name = tasty_model.predict(temp_image_path)
+                formatted_name, unformatted_name = format_dish_name(predicted_name)
+                st.session_state['predicted_dish_name'] = formatted_name
+                st.session_state['unformatted_dish_name'] = unformatted_name
+                st.session_state['prediction_done'] = True
+                st.success(f"Predicted Dish Name: {formatted_name}")
+            finally:
+                if os.path.exists(temp_image_path):
+                    os.remove(temp_image_path)
+
+# function to display dishes name drop down and saves user selected option in the session
+def dish_name_and_selection():
+    dish_options = ["Select an option"] + [
+        format_dish_name(name)[0] for name in processed_df["name"].unique()
+    ]
+
+    # Ensure dropdown uses the formatted predicted dish name
+    if st.session_state.get('reset_trigger', False):
+        st.session_state['selected_recipe'] = "Select an option"  # Reset state
+        st.session_state['reset_trigger'] = False  # Clear the reset trigger
+
+    # Dropdown is disabled only if a prediction is made and not reset
+    dropdown_disabled = st.session_state['prediction_done'] and not st.session_state['reset_done']
+    selected_recipe = st.selectbox(
+        "Select a dish name",
+        dish_options,
+        index=dish_options.index(st.session_state.get('selected_recipe', "Select an option")),
+        disabled=dropdown_disabled,
+        key="selected_recipe"
+    )
+
+# this function displays all the optional parameters using the options available in our limited recipe dataset (processed_df)
+# all options are single selection, except allergy type which is multi select
+def display_optional_parameters():
+    selected_cuisine = st.selectbox(
+        "Select a Cuisine (optional)",
+        ["Select an option"] + list(processed_df["cuisine"].unique()),
+        key="selected_cuisine"
+    )
+    selected_course = st.selectbox(
+        "Select a Course (optional)",
+        ["Select an option"] + list(processed_df["course"].unique()),
+        key="selected_course"
+    )
+    selected_diet_type = st.selectbox(
+        "Select a Diet Type (optional)",
+        ["Select an option"] + list(processed_df["diet"].unique()),
+        key="selected_diet_type"
+    )
+    selected_prep_time = st.selectbox(
+        "Select preparation time (optional)",
+        ["Select an option"] + list(processed_df['categorized_prep_time'].unique()),
+        key="selected_prep_time"
+    )
+    selected_allergies = st.multiselect(
+        "Allergy Information",
+        processed_df['allergen_type'].explode().unique(),
+        key="selected_allergies"
+    )
 
 #Find dish name in recipes and filter them based on user preferences using filter_recipe() function
 def filter_empty_option_and_df(df, dish_name, cuisine=None, course=None, diet=None, prep_time=None, allergen_type=None, debug=False): 
@@ -64,3 +187,86 @@ def format_instructions(instructions):
     formatted_instructions = "\n".join(f"- {instruction}" for instruction in instructions_list if instruction)
 
     return formatted_instructions
+
+# function to Format the recipe name for display for each recipe
+def format_recipe_details_and_display(row):
+    formatted_name, _ = format_dish_name(row.name)
+    st.markdown(f"### **{formatted_name}**")
+
+    # Display recipe details
+    st.markdown(f"**<span style='color:blue;'>Cuisine:</span>** {row.cuisine}", unsafe_allow_html=True)
+    st.markdown(f"**<span style='color:blue;'>Course:</span>** {row.course}", unsafe_allow_html=True)
+    st.markdown(f"**<span style='color:blue;'>Diet Type:</span>** {row.diet}", unsafe_allow_html=True)
+    st.markdown(f"**<span style='color:blue;'>Preparation Time:</span>** {row.prep_time} minutes", unsafe_allow_html=True)
+
+    # Format and display the ingredients
+    ingredients = row.cleaned_ingredients
+    formatted_ingredients = "\n".join(f"- {ingredient}" for ingredient in ingredients)
+    st.markdown("**<span style='color:blue;'>Ingredients:</span>**", unsafe_allow_html=True)
+    st.markdown(formatted_ingredients)
+
+    # Format and display allergens as a comma-separated list
+    allergens = ", ".join(row.allergens)
+    st.markdown(f"**<span style='color:blue;'>Allergens:</span>** {allergens}", unsafe_allow_html=True)
+
+    # Instructions
+    st.markdown("**<span style='color:blue;'>Instructions:</span>**", unsafe_allow_html=True)
+    if row.instructions:
+        formatted_instructions = format_instructions(row.instructions)
+        # Display formatted instructions
+        st.markdown(formatted_instructions)
+    else:
+        st.write("Instructions not available.")
+
+# display Feedback radio buttons after the recipe details are displayed
+def display_feedback_radio_button_and_store_feedback(row):
+    feedback_key = f"feedback_{row.Index}"
+    selected_feedback = st.radio(
+        f"Was this recipe helpful?",
+        options=["Select", "Helpful", "Not Related"],
+        index=0,
+        key=feedback_key
+    )
+
+    if selected_feedback != "Select":
+        st.session_state['feedback_dict'][row.Index] = selected_feedback
+
+# if user clicks on "Submit feedback and/or find another Recipe" button
+# process feedback and save it in a json file and 
+# update the weights of the feedback
+# refresh the screen
+def save_feedback_update_wt_refresh_screen():
+    feedback_data = {
+        "user_inputs": user_inputs,
+        "feedback": st.session_state['feedback_dict'],
+        "recommendations": recommended_recipes.to_dict(orient="records")
+    }
+
+    # File path for the feedback file
+    feedback_file_path = "models/user_feedback.json"
+
+    # Ensure the file and directory exist
+    os.makedirs(os.path.dirname(feedback_file_path), exist_ok=True)
+
+    try:
+        # Load existing feedback if the file exists
+        with open(feedback_file_path, "r") as f:
+            existing_feedback = json.load(f)
+    except FileNotFoundError:
+        # If the file doesn't exist, initialize with an empty list
+        existing_feedback = []
+
+    # Append new feedback data
+    existing_feedback.append(feedback_data)
+
+    # Save updated feedback back to the file
+    with open(feedback_file_path, "w") as f:
+        json.dump(existing_feedback, f, indent=4)
+
+    # Update the model with the feedback
+    feedback_model.update_weights(st.session_state['feedback_dict'])
+    
+    print("Session state before reset:", st.session_state)
+    # Reset everything, including the uploaded image
+    reset_session_state()
+    st.experimental_rerun()  # Ensure the UI refreshes completely
